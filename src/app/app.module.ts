fb535a42c8e931e7317e0900768be4a28dfed014
Diogo <diogonc@gmail.com>
2016-10-04 22:16:37 -0400

    fixing app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AccountComponent } from './account/account.component';
-import { AccountComponent } from './models/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent,
-    AccountComponent
   ],
   imports: [
     BrowserModule,
