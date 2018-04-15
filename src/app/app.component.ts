d2ca097206247545d33a5d2e044c281c40bacbf6
= <=>
2018-04-15 09:30:46 -0400

    fixing burguer on cellphone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,7 @@ export class AppComponent implements OnInit {
       const user = this.userRepository.getUser();
       this.user = user.login;
     }
+    this.carregarDropDown();
   }
 
   logout() {
@@ -73,5 +74,29 @@ export class AppComponent implements OnInit {
     const blob = new Blob([csvData], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
     window.open(url);
+  }
+
+  carregarDropDown() {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+
+          // Get the target from the "data-target" attribute
+          const target = $el.dataset.target;
+          const $target = document.getElementById(target);
+
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
   }
 }
